6ce4f3f65f098d9a974c888bbb9238125a065b31
litlighilit <litlighilit@foxmail.com>
2024-10-29 12:00:47 +0800

    impressed: better err repr; simpify some notation

--- MainForm.cs
+++ MainForm.cs
@@ -25,28 +25,64 @@ namespace file_manage
         protected void Error(string msg) => MessageBox.Show(msg);
         protected void Error(string msg, string title) => MessageBox.Show(msg, title);
 
+        protected bool TryGetDirectories(out string[] res, string path)
+        {
+            string title = "错误";
+            string msg;
+            try
+            {
+                res = Directory.GetDirectories(path);
+                return true;
+            }
+            catch (UnauthorizedAccessException exc)
+            {
+                msg = exc.Message;
+                title = "权限不足";
+            }
+            catch (DirectoryNotFoundException exc)
+            {
+                msg = exc.Message;
+                title = "目录不存在";
+            }
+            catch (PathTooLongException exc)
+            {
+                msg = exc.Message;
+                title = "路径过长，无法支持";
+            }
+            catch (NotSupportedException exc)
+            { 
+                msg = exc.Message;
+                msg += ' ';
+                msg += path;
+                title = "路径参数格式错误";
+            }
+            catch (Exception e)
+            {
+                msg = e.Message;
+                msg += '\n';
+                msg += e.GetType().ToString();
+            }
+            Error(msg, title);
+            res = new string[0];
+            return false;
+        }
         private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
         {
             e.Node.SelectedImageIndex = e.Node.ImageIndex;
-            string folderpath = e.Node.FullPath.ToString();
+            string folderpath = e.Node.FullPath;
             listViewItem.Items.Clear();
 
-            string[] subDirectories;
-            try
-            {
-                subDirectories = Directory.GetDirectories(folderpath);
-            } catch (System.UnauthorizedAccessException exc)
-            {
-                Error(exc.Message, "权限不足");
-                return;
-            }
+            var succ = TryGetDirectories(out string[] subDirectories, folderpath);
+            if (!succ) return;
             string[] files = Directory.GetFiles(folderpath);
             foreach (string subDirectory in subDirectories)
             {
-                DirectoryInfo name = new DirectoryInfo(subDirectory);
-                ListViewItem item = new ListViewItem(name.Name, 0);
-                //item.SubItems.Add("文件夹");
-                item.Tag = subDirectory;
+                var name = new DirectoryInfo(subDirectory);
+                var item = new ListViewItem(name.Name, 0)
+                {
+                    //SubItems.Add("文件夹");
+                    Tag = subDirectory
+                };
                 listViewItem.Items.Add(item);
             }
             foreach (string file in files)
@@ -62,9 +98,11 @@ namespace file_manage
                     image_index = 2;
                 }
 
-                ListViewItem item = new ListViewItem(Path.GetFileName(file), image_index);
-                item.Tag = file;
-                
+                ListViewItem item = new ListViewItem(Path.GetFileName(file), image_index)
+                {
+                    Tag = file
+                };
+
                 //item.SubItems.Add("文件");
                 listViewItem.Items.Add(item);
             }
@@ -72,50 +110,45 @@ namespace file_manage
 
         private void PopulateTreeView(string folderPath, TreeNode parentNode)
         {
-            try
+            // 获取当前文件夹的所有子文件夹
+            var succ = TryGetDirectories(out string[] subFolders, folderPath);
+            if (!succ) return;
+            if (subFolders == null || subFolders.Length == 0 ) return;
+            else
             {
-                // 获取当前文件夹的所有子文件夹
-                string[] subFolders = Directory.GetDirectories(folderPath);
-                if (subFolders == null || subFolders.Length == 0 ) { return; }
-                else
+                // 处理当前文件夹              
+                // 递归遍历子文件夹
+                foreach (var subFolder in subFolders)
                 {
-                    // 处理当前文件夹              
-                    // 递归遍历子文件夹
-                    foreach (string subFolder in subFolders)
+                    var directoryInfo = new DirectoryInfo(subFolder);
+                    var treeNode = new TreeNode(directoryInfo.Name, 0, 0);
+                    if (directoryInfo.Exists)
                     {
-                        DirectoryInfo directoryInfo = new DirectoryInfo(subFolder);
-                        TreeNode treeNode = new TreeNode(directoryInfo.Name, 0, 0);
-                        if (directoryInfo.Exists)
-                        {
-                            parentNode.Nodes.Add(treeNode);
-                        }
-                        PopulateTreeView(subFolder, treeNode);
+                        parentNode.Nodes.Add(treeNode);
                     }
+                    PopulateTreeView(subFolder, treeNode);
                 }
+            }
 
-            }
-            catch (UnauthorizedAccessException)
-            {
-                Console.WriteLine("访问被拒绝: " + folderPath);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("发生错误: " + ex.Message);
-            }
 
         }
+        protected static readonly string SubDirectoryDummyTag = "Dummy";
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
             DriveInfo[] drives = DriveInfo.GetDrives();
-            foreach (DriveInfo drive in drives)
+            foreach (var drive in drives)
             {
                 if (drive.DriveType == DriveType.Fixed) // 只添加固定磁盘驱动器
                 {
-                    TreeNode driveNode = treeViewDir.Nodes.Add(drive.Name, drive.Name, 2);
+                    var key = drive.Name;
+                    //var name = key.TrimEnd('/', '\\');  // 去除 分隔符后缀
+                    //var text = $"{drive.VolumeLabel} ({name})"; // 模仿 Windows Explorer 行为
+                    var text = key;
+                    TreeNode driveNode = treeViewDir.Nodes.Add(key, text, 2);
                     driveNode.Tag = drive.RootDirectory.FullName; // 保存路径信息到节点的 Tag 属性
-                    driveNode.Nodes.Add("Dummy"); // 添加一个虚拟子节点，表示有子文件夹
+                    driveNode.Nodes.Add(SubDirectoryDummyTag); // 添加一个虚拟子节点，表示有子文件夹
                 }
             }
         }
@@ -128,32 +161,23 @@ namespace file_manage
         private void treeView1_AfterExpand(object sender, TreeViewEventArgs e)
         {
             
-            if (e.Node.Nodes.Count == 1 && e.Node.Nodes[0].Text == "Dummy")
+            if (e.Node.Nodes.Count == 1 && e.Node.Nodes[0].Text == SubDirectoryDummyTag)
             {
                 e.Node.Nodes.Clear(); // 移除虚拟子节点
 
-                string nodePath = e.Node.Tag.ToString();
+                var nodePath = e.Node.Tag.ToString();
 
-                try
+                var succ = TryGetDirectories(out string[] subDirectories, nodePath);
+                if (!succ) return;
+                foreach (var subDirectory in subDirectories)
                 {
-                    string[] subDirectories = Directory.GetDirectories(nodePath);
-                    foreach (string subDirectory in subDirectories)
-                    {
-                        DirectoryInfo directoryInfo = new DirectoryInfo(subDirectory);
-                        TreeNode subNode = e.Node.Nodes.Add(directoryInfo.Name, directoryInfo.Name, 0);
-                        subNode.Tag = directoryInfo.FullName; // 保存路径信息到节点的 Tag 属性
-                        subNode.Nodes.Add("Dummy"); // 添加一个虚拟子节点，表示有子文件夹
-                    }
+                    var directoryInfo = new DirectoryInfo(subDirectory);
+                    TreeNode subNode = e.Node.Nodes.Add(directoryInfo.Name, directoryInfo.Name, 0);
+                    subNode.Tag = directoryInfo.FullName; // 保存路径信息到节点的 Tag 属性
+                    subNode.Nodes.Add(SubDirectoryDummyTag); // 添加一个虚拟子节点，表示有子文件夹
                 }
-                catch (UnauthorizedAccessException)
-                {
-                    // 处理访问被拒绝的情况
-                    MessageBox.Show("Access to a folder was denied.");
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("An error occurred: " + ex.Message);
-                }
+                //ListDirsToNode(nodePath, ref e.Node.Nodes);
+                
             }
         }
 
@@ -169,7 +193,8 @@ namespace file_manage
             else if (Directory.Exists(nodeName))
             {
                 listViewItem.Clear();
-                string[] directories = Directory.GetDirectories(nodeName);
+                var succ = TryGetDirectories(out string[] directories, nodeName);
+                if (!succ) return;
                 string[] files = Directory.GetFiles(nodeName);
                 foreach (string directory in directories)
                 {
