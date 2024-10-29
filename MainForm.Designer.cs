354c9ddc2de9344f384589546d045b91a2d966a0
litlighilit <litlighilit@foxmail.com>
2024-10-29 10:48:10 +0800

    init with basic functionality

---
+++ MainForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace file_manage
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.splitContainerDirItem = new System.Windows.Forms.SplitContainer();
+            this.treeViewDir = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listViewItem = new System.Windows.Forms.ListView();
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerDirItem)).BeginInit();
+            this.splitContainerDirItem.Panel1.SuspendLayout();
+            this.splitContainerDirItem.Panel2.SuspendLayout();
+            this.splitContainerDirItem.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainerDirItem
+            // 
+            this.splitContainerDirItem.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerDirItem.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerDirItem.Margin = new System.Windows.Forms.Padding(4);
+            this.splitContainerDirItem.Name = "splitContainerDirItem";
+            // 
+            // splitContainerDirItem.Panel1
+            // 
+            this.splitContainerDirItem.Panel1.Controls.Add(this.treeViewDir);
+            // 
+            // splitContainerDirItem.Panel2
+            // 
+            this.splitContainerDirItem.Panel2.Controls.Add(this.listViewItem);
+            this.splitContainerDirItem.Size = new System.Drawing.Size(1067, 600);
+            this.splitContainerDirItem.SplitterDistance = 309;
+            this.splitContainerDirItem.SplitterWidth = 5;
+            this.splitContainerDirItem.TabIndex = 0;
+            // 
+            // treeViewDir
+            // 
+            this.treeViewDir.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeViewDir.ImageIndex = 2;
+            this.treeViewDir.ImageList = this.imageList1;
+            this.treeViewDir.Location = new System.Drawing.Point(0, 0);
+            this.treeViewDir.Margin = new System.Windows.Forms.Padding(4);
+            this.treeViewDir.Name = "treeViewDir";
+            this.treeViewDir.SelectedImageIndex = 0;
+            this.treeViewDir.Size = new System.Drawing.Size(309, 600);
+            this.treeViewDir.TabIndex = 0;
+            this.treeViewDir.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterCheck);
+            this.treeViewDir.AfterExpand += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterExpand);
+            this.treeViewDir.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "imageres 004.png");
+            this.imageList1.Images.SetKeyName(1, "imageres 006.png");
+            this.imageList1.Images.SetKeyName(2, "disk.png");
+            // 
+            // listViewItem
+            // 
+            this.listViewItem.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewItem.HideSelection = false;
+            this.listViewItem.Location = new System.Drawing.Point(0, 0);
+            this.listViewItem.Margin = new System.Windows.Forms.Padding(4);
+            this.listViewItem.MultiSelect = false;
+            this.listViewItem.Name = "listViewItem";
+            this.listViewItem.Size = new System.Drawing.Size(753, 600);
+            this.listViewItem.SmallImageList = this.imageList2;
+            this.listViewItem.TabIndex = 0;
+            this.listViewItem.UseCompatibleStateImageBehavior = false;
+            this.listViewItem.View = System.Windows.Forms.View.List;
+            this.listViewItem.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listViewItem.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // imageList2
+            // 
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.SystemColors.InfoText;
+            this.imageList2.Images.SetKeyName(0, "imageres 004.png");
+            this.imageList2.Images.SetKeyName(1, "imageres 098.png");
+            this.imageList2.Images.SetKeyName(2, "imageres 166.png");
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 600);
+            this.Controls.Add(this.splitContainerDirItem);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.Text = "FileMgr";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainerDirItem.Panel1.ResumeLayout(false);
+            this.splitContainerDirItem.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerDirItem)).EndInit();
+            this.splitContainerDirItem.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainerDirItem;
+        private System.Windows.Forms.TreeView treeViewDir;
+        public System.Windows.Forms.ImageList imageList1;
+        public System.Windows.Forms.ImageList imageList2;
+        public System.Windows.Forms.ListView listViewItem;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections.Specialized;
+using System.Diagnostics;
+
+
+
+namespace file_manage
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            e.Node.SelectedImageIndex = e.Node.ImageIndex;
+            string folderpath = e.Node.FullPath.ToString();
+            listViewItem.Items.Clear();
+
+            string[] subDirectories = Directory.GetDirectories(folderpath);
+            string[] files = Directory.GetFiles(folderpath);
+            foreach (string subDirectory in subDirectories)
+            {
+                DirectoryInfo name = new DirectoryInfo(subDirectory);
+                ListViewItem item = new ListViewItem(name.Name, 0);
+                //item.SubItems.Add("文件夹");
+                item.Tag = subDirectory;
+                listViewItem.Items.Add(item);
+            }
+            foreach (string file in files)
+            {
+                int image_index = 1;
+                string fileExtension = Path.GetExtension(file).ToLower();
+                if (fileExtension == ".txt")
+                {
+                    image_index = 1;
+                }
+                else if(fileExtension ==".zip" || fileExtension == ".rar")
+                {
+                    image_index = 2;
+                }
+
+                ListViewItem item = new ListViewItem(Path.GetFileName(file), image_index);
+                item.Tag = file;
+                
+                //item.SubItems.Add("文件");
+                listViewItem.Items.Add(item);
+            }
+        }
+
+        private void PopulateTreeView(string folderPath, TreeNode parentNode)
+        {
+            try
+            {
+                // 获取当前文件夹的所有子文件夹
+                string[] subFolders = Directory.GetDirectories(folderPath);
+                if (subFolders == null || subFolders.Length == 0 ) { return; }
+                else
+                {
+                    // 处理当前文件夹              
+                    // 递归遍历子文件夹
+                    foreach (string subFolder in subFolders)
+                    {
+                        DirectoryInfo directoryInfo = new DirectoryInfo(subFolder);
+                        TreeNode treeNode = new TreeNode(directoryInfo.Name, 0, 0);
+                        if (directoryInfo.Exists)
+                        {
+                            parentNode.Nodes.Add(treeNode);
+                        }
+                        PopulateTreeView(subFolder, treeNode);
+                    }
+                }
+
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine("访问被拒绝: " + folderPath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("发生错误: " + ex.Message);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                if (drive.DriveType == DriveType.Fixed) // 只添加固定磁盘驱动器
+                {
+                    TreeNode driveNode = treeViewDir.Nodes.Add(drive.Name, drive.Name, 2);
+                    driveNode.Tag = drive.RootDirectory.FullName; // 保存路径信息到节点的 Tag 属性
+                    driveNode.Nodes.Add("Dummy"); // 添加一个虚拟子节点，表示有子文件夹
+                }
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Console.WriteLine(e.ToString());
+        }
+
+        private void treeView1_AfterExpand(object sender, TreeViewEventArgs e)
+        {
+            
+            if (e.Node.Nodes.Count == 1 && e.Node.Nodes[0].Text == "Dummy")
+            {
+                e.Node.Nodes.Clear(); // 移除虚拟子节点
+
+                string nodePath = e.Node.Tag.ToString();
+
+                try
+                {
+                    string[] subDirectories = Directory.GetDirectories(nodePath);
+                    foreach (string subDirectory in subDirectories)
+                    {
+                        DirectoryInfo directoryInfo = new DirectoryInfo(subDirectory);
+                        TreeNode subNode = e.Node.Nodes.Add(directoryInfo.Name, directoryInfo.Name, 0);
+                        subNode.Tag = directoryInfo.FullName; // 保存路径信息到节点的 Tag 属性
+                        subNode.Nodes.Add("Dummy"); // 添加一个虚拟子节点，表示有子文件夹
+                    }
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    // 处理访问被拒绝的情况
+                    MessageBox.Show("Access to a folder was denied.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred: " + ex.Message);
+                }
+            }
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            ListViewItem selectedListItem = listViewItem.SelectedItems[0];
+            string nodeName = selectedListItem.Tag.ToString(); // 获取选中节点的文本
+            string itemName = selectedListItem.Text;
+            if (File.Exists(nodeName))
+            {
+                Process.Start(nodeName);
+            }
+            else if (Directory.Exists(nodeName))
+            {
+                listViewItem.Clear();
+                string[] directories = Directory.GetDirectories(nodeName);
+                string[] files = Directory.GetFiles(nodeName);
+                foreach (string directory in directories)
+                {
+                    DirectoryInfo foo = new DirectoryInfo(directory);
+                    ListViewItem item = new ListViewItem(foo.Name, 0);
+                    item.Tag = foo.FullName;
+                    listViewItem.Items.Add(item);
+                }
+                foreach (string file in files)
+                {
+                    int image_index = 1;
+                    string fileExtension = Path.GetExtension(file).ToLower();
+                    if (fileExtension == ".txt")
+                    {
+                        image_index = 1;
+                    }
+                    else if (fileExtension == ".zip" || fileExtension == ".rar")
+                    {
+                        image_index = 2;
+                    }
+
+                    ListViewItem item = new ListViewItem(Path.GetFileName(file), image_index);
+                    item.Tag = file;
+
+                    //item.SubItems.Add("文件");
+                    listViewItem.Items.Add(item);
+                }
+            }
+        }
+
+        private void treeView1_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+
+
+        }
+    }
+}
